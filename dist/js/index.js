e6b368f0bfda27e7071c258569d88ecb26137f3b
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-09-30 11:24:57 +0700

    responsive navbar

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,4 +7,20 @@ const openUserProfileMenu = () => {
   });
 };
 
+const sidebarButton = document.getElementById("bar-button");
+const sidebarButton2 = document.getElementById("bar-button-2");
+const sidebar = document.getElementById("sidebar");
+
+const openSidebar = () => {
+  sidebar.classList.toggle("hidden");
+};
+
 openUserProfileMenu();
+sidebarButton.addEventListener("click", () => {
+  console.log("klik");
+  openSidebar();
+});
+sidebarButton2.addEventListener("click", () => {
+  console.log("klik");
+  openSidebar();
+});
